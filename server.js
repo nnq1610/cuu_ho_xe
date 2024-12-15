9f0ba9dca030e39bcb4c96ba63e19891306a5cbe
quangnn <quangboybin@gmail.com>
2024-12-15 23:24:07 +0700

    fix may ham linh tinh

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const PORT = process.env.PORT || 5050;
 const server = app.listen( PORT, () => {
     console.log(`Server start with PORT::${PORT} !!!`);
 })
-
-
 process.on('SIGINT', () => {
     server.close(() => console.log(`Exit Server Express`));
 })
